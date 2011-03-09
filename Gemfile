7f4ec78e530247a197f6340dddb4d826ca640570
David Wilkie <dwilkie@gmail.com>
2011-03-09 21:29:44 +0700

    added rails project

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+
+group :development do
+  gem 'sqlite3-ruby', :require => 'sqlite3'
+end
+
+group :development, :test do
+  gem 'ruby-debug19'
+  gem 'rspec-rails', '>= 2.5'
+end
+
+group :test do
+  gem "test-unit"
+  gem 'cucumber-rails'
+  gem 'capybara'
+  gem 'factory_girl_rails'
+  gem 'pickle', '>= 0.4.4'
+  gem 'spork', '>=0.9.0.rc2'
+  gem 'fakeweb'
+  gem 'launchy'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wherewhat::Application.config.secret_token = '5ac87dfb53777efb987322ab028016cadf1c7c739c686bc1a4d6d0ed310696902a654e6d1f3413028445de7fa272c154b90cf2c7956e0a577c5e1f90717d088f'
